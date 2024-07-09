e6a8fed7a5a80bbcbb77db1d7f0248a649bd525e
Shawn <158554907+Shawn983@users.noreply.github.com>
2024-07-09 17:02:44 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,44 @@
+// basics
+let myName = "Shawn";
+const myAge = 18;
+let myPhone = 1234567;
+myPhone = 7654321;
+let myArr = [1, 2, 3, 4, 5];
+let isHuman = true; // true or false
 
+console.log(myName);
+console.log(myAge);
+console.log(myPhone);
+console.log(myArr);
+console.log(isHuman);
+
+// Playing with numbers
+let a = (5 * 7) * 2;
+let b = (5 - 7) / 3;
+let c = 5 ** 7 / 3;
+let d = 123 % 45
+console.log(a, b, c ,d);
+
+let e = Math.random();
+let f = Math.round(123.456789);
+let g = Math.cos(Math.PI)
+console.log(e);
+console.log(f);
+console.log("cos(pi) = " + g);
+console.log("e-squared = " + h);
+
+// Strings
+let firstName = "Andrew";
+let lastName = "Tay";
+let fullName = firstName + " " + lastName;
+let nameLength = fullname.length;
+console.log(fullName + ", Length: " + nameLength);
+
+let language = "Javascript";
+console.log(language[0]);
+console.log(language.substring(2, 5));
+console.log(language.toUpperCase());
+console.log(language.replace(" Java", "Type"));
+console.log(language.includes(" Java"));
+// use string literals
+console.log(' But I also like ${language.toLowerCase()}!')
